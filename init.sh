cba394bae5dc303fa8d60636be09c0a556304d54
Jeongseup <seup87@gmail.com>
2023-05-09 06:02:53 +0900

    update init.sh

--- init.sh
+++ init.sh
@@ -9,16 +9,17 @@ STAKING_AMOUNT="1000000000stake"
 
 NAMESPACE_ID=$(openssl rand -hex 8)
 echo $NAMESPACE_ID
-DA_BLOCK_HEIGHT=$(curl https://rpc-blockspacerace.pops.one/block | jq -r '.result.block.header.height')
+DA_BLOCK_HEIGHT=$(curl https://localhost:26657/block | jq -r '.result.block.header.height')
 echo $DA_BLOCK_HEIGHT
 
 NODE_DAEMON=denomchaind
 ignite chain build
-recipesd tendermint unsafe-reset-all
-recipesd init $VALIDATOR_NAME --chain-id $CHAIN_ID
 
-recipesd keys add $KEY_NAME --keyring-backend test
-recipesd add-genesis-account $KEY_NAME $TOKEN_AMOUNT --keyring-backend test
-recipesd gentx $KEY_NAME $STAKING_AMOUNT --chain-id $CHAIN_ID --keyring-backend test
-recipesd collect-gentxs
-recipesd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://localhost:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT
+$NODE_DAEMON tendermint unsafe-reset-all
+$NODE_DAEMON init $VALIDATOR_NAME --chain-id $CHAIN_ID
+
+$NODE_DAEMON keys add $KEY_NAME --keyring-backend test
+$NODE_DAEMON add-genesis-account $KEY_NAME $TOKEN_AMOUNT --keyring-backend test
+$NODE_DAEMON gentx $KEY_NAME $STAKING_AMOUNT --chain-id $CHAIN_ID --keyring-backend test
+$NODE_DAEMON collect-gentxs
+$NODE_DAEMON start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://localhost:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT
